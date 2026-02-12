e0a867cb2ce90fb1f7f0205719d2fc26696966d3
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-12 14:24:24 +0000

    text page template

---
+++ page-templates/text-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Text Page
+ *
+ * @package cb-coda2026
+ */
+
+defined( 'ABSPATH' ) || exit;
+get_header();
+
+?>
+<main id="main" class="text-page">
+	<div class="post-title">
+		<div class="id-container px-4 px-md-5">
+			<div class="row">
+				<div class="col-md-9">
+					<h1 class="pt-1"><?= esc_html( get_the_title() ); ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="id-container">
+		<div class="row post-content-row mb-5">
+			<div class="col-md-3"></div>
+			<div class="col-md-9 text-white post-content px-4 px-md-5 ps-md-0 pe-md-5">
+				<?php
+				echo apply_filters( 'the_content', get_the_content() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				?>
+			</div>
+		</div>
+	</div>
+</main>
+<?php
+get_footer();
+?>
