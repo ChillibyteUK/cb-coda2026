87f4443449069f554af33324e78d6b5034408346
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-12 11:10:01 +0000

    optional header subtitle

--- blocks/cb-service-page-header.php
+++ blocks/cb-service-page-header.php
@@ -15,11 +15,17 @@ $balance = get_field( 'balance' ) ? 'text-wrap: balance;' : '';
 	<div class="has-lime-1000-border-top has-lime-1000-border-bottom mt-4">
 		<h1 class="id-container px-4 px-md-5 fs-800 fw-light has-lime-1100-color lh-tightest pt-2 pb-1"><?= esc_html( get_field( 'title' ) ); ?></h1>
 	</div>
+	<?php
+	if ( get_field( 'subtitle' ) ) {
+		?>
 	<div class="has-lime-1000-border-bottom mb-4">
 		<div class="id-container px-4 px-md-5">
-			<h1 class="fw-light has-neutral-700-color fs-500 lh-tightest pt-2 pb-1"><?= esc_html( get_field( 'subtitle' ) ); ?></h1>
+			<h2 class="fw-light has-neutral-700-color fs-500 lh-tightest pt-2 pb-1"><?= esc_html( get_field( 'subtitle' ) ); ?></h2>
 		</div>
 	</div>
+		<?php
+	}
+	?>
 	<div class="id-container px-4 px-md-5 pt-1 pb-5">
 		<div class="fs-700 has-lime-1000-color w-constrained pb-5" style="--width: <?= esc_attr( $width ); ?>; <?= esc_attr( $balance ); ?>">
 			<?= wp_kses_post( get_field( 'content' ) ); ?>
