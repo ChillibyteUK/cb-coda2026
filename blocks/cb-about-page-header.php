6c7c0a69888f06b02580cbc7039c1695df97898b
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-13 12:30:54 +0000

    about header

--- blocks/cb-about-page-header.php
+++ blocks/cb-about-page-header.php
@@ -24,7 +24,7 @@ $bg = get_query_var( 'background', get_field( 'background' ) );
         <div class="id-container px-4 px-md-5">
             <h1><?= wp_kses_post( get_field( 'title' ) ); ?></h1>
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-8">
                     <div class="cb-about-page-header__intro-text fw-light"><?= wp_kses_post( get_field( 'intro_text' ) ); ?></div>
                 </div>
             </div>
