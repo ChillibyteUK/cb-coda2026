ca932e40b3a7b147d2f071fde17a7688cc4556ad
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-16 12:06:01 +0000

    what we delivered

--- blocks/cb-what-we-delivered.php
+++ blocks/cb-what-we-delivered.php
@@ -12,31 +12,25 @@ defined( 'ABSPATH' ) || exit;
 	<div class="delivered__container pb-5">
 		<div class="delivered__header">
 			<div class="id-container pt-1 px-4 px-md-5">
-				<h2 class="has-lime-1100-color">WHAT WE DELIVERED</h2>
+				<h2>WHAT WE DELIVERED</h2>
 			</div>
 		</div>
         <div class="delivered__grid-wrapper">
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-                Full project management and onsite coordination
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Creative direction and meeting identity design 
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Event branding across environment, communications, and materials 
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Opening video with motion graphics 
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Branded content and transition animations 
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Live PowerPoint support throughout 
-			</div>
-			<div class="id-container px-4 px-md-5 delivered__item" style="display: contents;" data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
-				Complete event set, staging, and technical production 
-			</div>
+			<?php
+			$delay = 0;
+			while ( have_rows( 'deliverables' ) ) {
+				the_row();
+				$item = get_sub_field( 'item' );
+				?>
+				<div class="delivered__item">
+					<div class="id-container px-4 px-md-5"  data-aos="fade-up" data-aos-delay="<?= esc_attr( $delay ); ?>">
+						<?= esc_html( $item ); ?>
+					</div>
+				</div>
+				<?php
+				$delay += 100;
+			}
+			?>
 		</div>
 	</div>
 </section>
