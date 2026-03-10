c4e3d5a5cee78c6a232b62c9fbaeb064835e84c9
ChillibyteUK <dan@chillibyte.co.uk>
2026-03-10 16:42:33 +0000

    contact form

--- blocks/cb-contact-form.php
+++ blocks/cb-contact-form.php
@@ -11,14 +11,14 @@ defined( 'ABSPATH' ) || exit;
 $block_id = $block['id'] ?? '';
 
 ?>
-<section id="<?php echo esc_attr( $block_id ); ?>" class="cb-contact-form">
-	<div class="id-container px-4 px-md-5">
-		<h2 class="cb-contact-form__title pt-4 pb-3">
-			<?= esc_html( get_field( 'title' ) ); ?>
-		</h2>
+<section class="cb-contact-form">
+	<div class="has-lime-1000-border-top has-lime-1000-border-bottom mt-4">
+		<h1 class="id-container px-4 px-md-5 fs-850 fw-light has-lime-1100-color lh-tightest pt-2 pb-1"><?= esc_html( get_field( 'title' ) ); ?></h1>
+	</div>
+	<div class="id-container px-4 px-md-5 pt-5 pb-5">
 		<div class="row">
 			<div class="col-md-6">
-				<div class="cb-contact-form__intro mb-4">
+				<div class="fs-700 fw-light has-lime-1000-color w-constrained pb-5">
 					<?= wp_kses_post( get_field( 'intro' ) ); ?>
 				</div>
 			</div>
